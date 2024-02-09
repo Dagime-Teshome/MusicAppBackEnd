d69a6bfe5f06982571475c6cf33647eb1a7664ef
Dagime Teshome <dagimeteshome1@gmail.com>
2024-02-09 07:44:39 +0300

    add new dist file

--- controllers/stats_controller.js
+++ controllers/stats_controller.js
@@ -2,13 +2,8 @@ const StatRouter = require("express").Router();
 const { generateStats } = require("../utils/helpers");
 
 StatRouter.get("/", async (req, res) => {
-  try {
-    const stats = await generateStats();
-    res.json(stats);
-  } catch (error) {
-    console.error("Error processing statistics:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+  const stats = await generateStats();
+  res.status(200).json(stats);
 });
 
 module.exports = StatRouter;
